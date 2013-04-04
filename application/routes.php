9b64bfe970ac495427627b843f3e03cad9b1c409
Steven Bandola <bandola.steven@gmail.com>
2013-04-03 23:07:54 -0600

    updated documentation

--- application/routes.php
+++ application/routes.php
@@ -37,6 +37,13 @@ Route::get('/', function()
 	return View::make('home.index');
 });
 
+Route::get('home', function()
+{
+	return View::make('home.dashboard');
+});
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Application 404 & 500 Error Handlers
